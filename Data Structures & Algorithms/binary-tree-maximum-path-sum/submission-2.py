d900aafd02be400ca800d749b83a7deb92092e35
nishantchoudhary6835-del <nishantchoudhary6835@gmail.com>
2026-09-09 22:14:37 +0530

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            # Path passing through current node
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+
+            # Return the best single branch to parent
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return res[0]
